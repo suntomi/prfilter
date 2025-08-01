e814421e6820ea00dfd7a9ca46b971f9fd755375
Takehiro Iyatomi <iyatomi@gmail.com>
2025-08-01 12:04:59 +0900

    use printf for passing exactly same input to jq (echo breaks with linefeed)

--- entrypoint.sh
+++ entrypoint.sh
@@ -1,10 +1,15 @@
 #!/bin/sh
+set -eo pipefail
 TYPE="$1"
 COMMAND="$2"
 DATA="$3"
+# log to stderr
+echo "${TYPE} ${COMMAND}" >&2
 if [ "${COMMAND}" = "match" ]; then
-  author=$(echo "${DEPLO_MODULE_OPTION_STRING}" | jq -r '.author')
-  input=$(echo "${DATA}" | jq -r '.event.pull_request.user.login')
+  author=$(printf '%s' "${DEPLO_MODULE_OPTION_STRING}" | jq -r '.author')
+  echo "author: ${author}" >&2
+  input=$(printf '%s' "${DATA}" | jq -r '.event.pull_request.user.login')
+  echo "author from input: ${input}" >&2
   if [ "${input}" = "${author}" ]; then
     echo "{\"user\": \"${author}\"}"
     exit 0
